23979ee877113baa66088d722c8189fbf9a9916b
Larry Tseng <lt222888@gmail.com>
2017-06-24 12:04:08 -0400

    Initial Commit

---
+++ BluetoothArduino/BTManager.swift
@@ -0,0 +1,157 @@
+//
+//  BTManager.swift
+//  BluetoothArduino
+//
+//  Created by Larry on 6/23/17.
+//  Copyright © 2017 Larry Tseng. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+protocol BTManagerDelegate {
+    func updateStatusMessage(message: String)
+    func btConnectionIsReady()
+    func btConnectionIsNotReady()
+}
+
+class BTManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var delegate: BTManagerDelegate? = nil
+    
+    var centralManager: CBCentralManager!
+    var arduinoPeripheral: CBPeripheral?
+    var arduinoCharacteristic: CBCharacteristic?
+    
+    let arduinoUUID: CBUUID = CBUUID(string: "4651FAB4-8C39-6212-40EE-91B6C0696FEC")
+    let serviceUUID: CBUUID = CBUUID(string: "FFE0")
+    let characteristicUUID: CBUUID = CBUUID(string: "FFE1")
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    // MARK: - Methods
+    
+    func sendData(data: Int8) {
+        var data = data
+        let packedData: Data = Data(bytes: &data, count: 1)
+        arduinoPeripheral?.writeValue(packedData, for: arduinoCharacteristic!, type: .withoutResponse)
+    }
+    
+    func startScanning() {
+        if (!centralManager.isScanning || arduinoPeripheral != nil) {
+            if (centralManager.state == .poweredOn) {
+                centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
+                delegate?.updateStatusMessage(message: "Scanning...")
+            }
+        }
+    }
+    
+    func cancelScanning() {
+        if arduinoPeripheral != nil {
+            centralManager.cancelPeripheralConnection(arduinoPeripheral!)
+        }
+        if centralManager.isScanning {
+            centralManager.stopScan()
+        }
+        delegate?.updateStatusMessage(message: "**Canceled scanning.**")
+    }
+    
+    // MARK: - CBCentralManagerDelegate
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        var message: String;
+        
+        switch central.state {
+        case .poweredOff:
+            message = "Turn on Bluetooth!"
+            break
+        case .resetting:
+            message = "Resetting..."
+            break
+        case .unauthorized:
+            message = "Unauthorized to use BLE."
+            break
+        case .unknown:
+            message = "Something went wrong with BLE."
+            break
+        case .unsupported:
+            message = "BLE not supported."
+            break
+        case .poweredOn:
+            message = "Ready to go! Scanning..."
+            central.scanForPeripherals(withServices: [serviceUUID], options: nil)
+            break
+        }
+        delegate?.updateStatusMessage(message: message)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        delegate?.updateStatusMessage(message: "Found: \(peripheral.name!)")
+        if let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            if peripheralName == "BT05" {
+                delegate?.updateStatusMessage(message: "Found \(peripheralName)! Connecting... RSSI: \(RSSI)")
+                
+                arduinoPeripheral = peripheral
+                arduinoPeripheral!.delegate = self
+                
+                central.connect(arduinoPeripheral!, options: nil)
+                
+                if central.isScanning {
+                    central.stopScan()
+                }
+            }
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        delegate?.updateStatusMessage(message: "\(peripheral.name!) connected!")
+        peripheral.discoverServices([serviceUUID])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        delegate?.updateStatusMessage(message: "**Error: \(peripheral.name!) disconnected!**")
+        arduinoPeripheral = nil
+        arduinoCharacteristic = nil
+        delegate?.btConnectionIsNotReady()
+        if error != nil {
+            delegate?.updateStatusMessage(message: "*Error: \(error!.localizedDescription)")
+        }
+    }
+    
+    // MARK: - CBPeripheral
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        if error != nil {
+            delegate?.updateStatusMessage(message: "**Error: \(error!.localizedDescription).**")
+        }
+        
+        delegate?.updateStatusMessage(message: "Found \(peripheral.name!) services.")
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics([characteristicUUID], for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        if error != nil {
+            delegate?.updateStatusMessage(message: "**Error: \(error!.localizedDescription).**")
+        }
+        
+        delegate?.updateStatusMessage(message: "Found \(peripheral.name!) characteristics.")
+        
+        for characteristic in service.characteristics! {
+            if characteristic.uuid == characteristicUUID {
+                arduinoCharacteristic = characteristic
+                delegate?.updateStatusMessage(message: "All set!")
+                delegate?.btConnectionIsReady()
+            }
+        }
+    }
+    
+}
---
+++ BluetoothArduino/ViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ViewController.swift
+//  BluetoothArduino
+//
+//  Created by Larry on 6/22/17.
+//  Copyright © 2017 Larry Tseng. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+enum Direction {
+    case Forward, Backward
+}
+
+class ViewController: UIViewController, BTManagerDelegate {
+    
+    let btManager: BTManager = BTManager()
+    let motionManager: CMMotionManager = CMMotionManager()
+    var timer: Timer!
+    
+    // MARK: - Speed Slider
+    
+    @IBOutlet weak var speedSlider: UISlider!
+    
+    var lastSpeed: Int8 = 0
+    @IBAction func sliderValueChange(_ sender: Any) {
+        let currentSpeed = Int8(speedSlider.value)
+        if currentSpeed != lastSpeed {
+            setSpeed(speed: currentSpeed)
+        }
+        lastSpeed = currentSpeed
+    }
+    
+    // MARK: Gyro Mode
+    
+    var gyroMode: Bool = false
+    
+    @IBAction func toggleGyroMode(_ sender: Any) {
+        gyroMode = !gyroMode
+        
+        if gyroMode {
+            print("Starting gyro mode!")
+            //motionManager.startGyroUpdates()
+            motionManager.startDeviceMotionUpdates()
+            readGyroOutput()
+        }
+        
+        if !gyroMode {
+            //motionManager.stopGyroUpdates()
+            motionManager.stopDeviceMotionUpdates()
+        }
+    }
+    
+    @IBOutlet weak var gyroStatus: UILabel!
+    
+    @objc func readGyroOutput() {
+        if let data = motionManager.deviceMotion?.attitude.pitch {
+            print(data)
+        }
+    }
+    
+    // MARK: - Buttons
+    
+    @IBAction func emergencyStopButton(_ sender: UIButton) {
+        stop()
+    }
+    
+    @IBOutlet weak var switchDirectionButton: UIButton!
+    
+    @IBAction func switchDirectionButton(_ sender: Any) {
+        if direction == .Forward {
+            setSpeed(speed: 101)
+            stop()
+            directionLabel.text = "Backwards"
+            switchDirectionButton.backgroundColor = .red
+            direction = .Backward
+        } else if direction == .Backward {
+            setSpeed(speed: 102)
+            stop()
+            directionLabel.text = "Forwards"
+            switchDirectionButton.backgroundColor = .green
+            direction = .Forward
+        }
+    }
+    
+    @IBAction func connectButton(_ sender: Any) {
+        btManager.startScanning()
+    }
+    
+    @IBAction func disconnectButton(_ sender: Any) {
+        btManager.cancelScanning()
+    }
+    
+    // MARK: - Status Message
+    
+    @IBOutlet weak var statusMessage: UITextView!
+    
+    @IBAction func clearStatusMessageButton(_ sender: Any) {
+        statusMessage.text = ""
+    }
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBOutlet weak var directionLabel: UILabel!
+    
+    var direction: Direction = Direction.Forward
+    
+    override func viewDidLoad() {
+        btManager.delegate = self
+        motionManager.gyroUpdateInterval = 0.5
+        motionManager.deviceMotionUpdateInterval = 0.5
+        btConnectionIsNotReady()
+        speedLabel.text = "0";
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(readGyroOutput), userInfo: nil, repeats: true)
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func update() {
+        
+    }
+    
+    // MARK: - BTManagerDelegate
+    
+    func updateStatusMessage(message: String) {
+        statusMessage.insertText(message + "\n")
+        print(message)
+    }
+    
+    func btConnectionIsReady() {
+        speedSlider.isEnabled = true
+        switchDirectionButton.isEnabled = true
+    }
+    
+    func btConnectionIsNotReady() {
+        speedSlider.isEnabled = false
+        switchDirectionButton.isEnabled = false
+    }
+    
+    // MARK: - Methods
+    
+    func setSpeed(speed: Int8) {
+        if btManager.arduinoPeripheral != nil && btManager.arduinoCharacteristic != nil {
+            btManager.sendData(data: speed)
+        }
+        speedLabel.text = "\(speed)"
+    }
+    
+    func stop() {
+        setSpeed(speed: 0)
+        speedSlider.setValue(0, animated: false)
+    }
+}
